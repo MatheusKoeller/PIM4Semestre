eee3312a6bb35576802810226f7824540ee1b4e6
Matheus <matheus.macao@amee.com.br>
2023-10-27 16:44:15 -0300

    calculo de desconto

--- Language School C#/Login/DAL/HoleriteDAO.cs
+++ Language School C#/Login/DAL/HoleriteDAO.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Windows.Forms;
 using Login.DAL;
 using Login.Modelo; // Certifique-se de importar as classes corretas
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 public class HoleriteDAO
 {
@@ -22,29 +23,29 @@ public class HoleriteDAO
             string query = "SELECT F.ID, F.nome, C.nome_cargo AS NomeCargo, C.salario " +
                             "FROM funcionarios AS F " +
                             "INNER JOIN cargos AS C ON F.cargo = C.ID";
-            string nomesCargos = "";
+
             using (SqlCommand command = new SqlCommand(query, connection))
             {
                 List<Holerite> holerites = new List<Holerite>();
 
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
-
                     while (reader.Read())
                     {
                         Cargo cargo = new Cargo
                         {
-                      
-                            Salario = reader.GetDecimal(3)
+                            Salario = reader.GetDecimal(3),
+                            NomeCargo = reader.GetString(2)
                         };
 
+                        decimal valorHolerite = CalcularHolerite(cargo);
 
                         Holerite holerite = new Holerite
                         {
                             FuncionarioID = reader.GetInt32(0),
                             NomeFuncionario = reader.GetString(1),
-                            NomeCargo = reader.GetString(2), // Nome do cargo
-                            ValorHolerite = CalcularHolerite(cargo)
+                            NomeCargo = cargo.NomeCargo,
+                            ValorHolerite = valorHolerite
                         };
 
                         holerites.Add(holerite);
@@ -55,6 +56,7 @@ public class HoleriteDAO
             }
         }
     }
+
 
 
 
@@ -85,10 +87,39 @@ public class HoleriteDAO
 
     private decimal CalcularHolerite(Cargo cargo)
     {
-        // Implemente o cálculo do holerite com base nas informações do cargo.
-        // Adicione suas regras de cálculo aqui, como descontos, adicionais, etc.
-        // Exemplo de cálculo simples (salário bruto do cargo):
-        return cargo.Salario;
+        decimal salario = cargo.Salario;
+        decimal salario2 = cargo.Salario;
+        decimal salario3 = cargo.Salario;
+        string nomeCargo = cargo.NomeCargo;
+        decimal valorHolerite = salario;
+       
+           if (salario <= 1320.00m)
+        {
+            valorHolerite -= Math.Round(salario * 0.075m, 2); // Desconto de 7.5%
+        }
+        else if (salario <= 2571.29m)
+        {
+            valorHolerite -= Math.Round(salario * 0.09m, 2); // Desconto de 9.0%
+        }
+        else if (salario <= 3856.94m)
+        {
+            valorHolerite -= Math.Round(salario * 0.12m, 2); // Desconto de 12.0%
+        }
+        else if (salario <= 7507.49m)
+        {
+            valorHolerite -= Math.Round(salario * 0.14m, 2); // Desconto de 14.0%
+        }
+
+        // Adicionar descontos para VR (10%) e VA (6%)
+        decimal descontoVR = Math.Round(salario2 * 0.10m, 2); // Desconto de 10% do VR
+        decimal descontoVA = Math.Round(salario3 * 0.06m, 2); // Desconto de 6% do VA
+        
+
+        valorHolerite -= descontoVR;
+        valorHolerite -= descontoVA;
+
+        return Math.Round(valorHolerite, 2);
+        
     }
     private string RetornaNomeCargo(Cargo cargo)
     {
